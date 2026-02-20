53ce5ef7b301c5a518daba429a11b4c0809c6b97
boztopuz <boztopuz@gmail.com>
2026-02-20 10:51:01 +0300

    [BE-8307]: clean parameter changed as optional

--- main.rb
+++ main.rb
@@ -21,6 +21,9 @@ $project_path = env_has_key("AC_PROJECT_PATH")
 $scheme = env_has_key("AC_SCHEME")
 $repository_path = ENV["AC_REPOSITORY_DIR"]
 
+# AC_CLEAN_BUILD - component.yaml: "true" | "false". Default: "true"
+$clean_build = ENV["AC_CLEAN_BUILD"].to_s != "false"
+
 $project_full_path = $repository_path ? (Pathname.new $repository_path).join($project_path) : $project_path
 
 $configuration_name = (ENV["AC_CONFIGURATION_NAME"] != nil && ENV["AC_CONFIGURATION_NAME"] !="") ? ENV["AC_CONFIGURATION_NAME"] : nil
@@ -441,9 +444,9 @@ def archive()
     key_path = env_has_key("AC_AUTOSIGN_CRED_PATH")
     key_id = env_has_key("AC_AUTOSIGN_KEY")
     issuer_id = env_has_key("AC_AUTOSIGN_ISSUER_ID")
-    command = "xcodebuild -allowProvisioningUpdates -authenticationKeyPath #{key_path} -authenticationKeyID #{key_id} -authenticationKeyIssuerID #{issuer_id} -scheme \"#{$scheme}\" clean archive -archivePath \"#{$archive_path}\" -derivedDataPath \"#{$temporary_path}/DerivedData\" -destination \"generic/platform=iOS\""
+    command = "xcodebuild -allowProvisioningUpdates -authenticationKeyPath #{key_path} -authenticationKeyID #{key_id} -authenticationKeyIssuerID #{issuer_id} -scheme \"#{$scheme}\" #{$clean_build ? 'clean ' : ''}archive -archivePath \"#{$archive_path}\" -derivedDataPath \"#{$temporary_path}/DerivedData\" -destination \"generic/platform=iOS\""
   else
-    command = "xcodebuild -scheme \"#{$scheme}\" clean archive -archivePath \"#{$archive_path}\" -derivedDataPath \"#{$temporary_path}/DerivedData\" -destination \"generic/platform=iOS\""
+    command = "xcodebuild -scheme \"#{$scheme}\" #{$clean_build ? 'clean ' : ''}archive -archivePath \"#{$archive_path}\" -derivedDataPath \"#{$temporary_path}/DerivedData\" -destination \"generic/platform=iOS\""
   end
 
   if $is_sign_available
